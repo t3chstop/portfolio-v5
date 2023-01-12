9f56daec9832f221880048b9ad32c761f8e37c00
t3chstop <84115793+t3chstop@users.noreply.github.com>
2023-01-11 20:54:18 -0800

    fix responsiveness of navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export function Header() {
     const handleToggle = () => setShow(!show);
   
     return (
-            <Box textAlign='left' pt='36' pl='20'>
+            <Box textAlign='left' pt='36' pl={[0, 10, 20]}>
                 <Heading as='h1' fontWeight='bold' fontSize='8xl' color='white'>Hey, I'm <Heading as='span' fontWeight='bold' fontSize='8xl' color='blue.400'>Siddarth</Heading></Heading>
                 <Text fontWeight='light' fontSize='24px' pt='16px' maxWidth='xl' color='white'>Full-Stack web developer, 3D artist and robotics enthusiast</Text>
             </Box>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,37 +3,43 @@ import {
     Breadcrumb,
     BreadcrumbItem,
     BreadcrumbLink,
-    BreadcrumbSeparator,
     Flex,
-    Heading,
     Link,
     Spacer,
-    Text,
+    Hide,
+    Box,
   } from '@chakra-ui/react'
 
 
 export function Navbar() {
     return (
-        <Flex alignContent='right' pt='9' pr='20' pl='20'>
-            <Link href='#' color='white' fontSize='2xl'>t3chstop.github.io</Link>
-            <Spacer />
-            <Breadcrumb separator='' fontSize='2xl' color='white'>
-                <BreadcrumbItem isCurrentPage>
-                    <BreadcrumbLink href='#'>Home</BreadcrumbLink>
-                </BreadcrumbItem>
+        <Box alignContent='right' pt='9' pr='20' pl={[0, 10, 20]} id='nav'>
+            <Flex display={{base: 'none', sm:'none', md:'none', lg:'flex'}}>
+                <Link href='#' color='white' fontSize='2xl'>t3chstop.github.io</Link>
+                <Spacer />
+                <Breadcrumb separator='' fontSize='2xl' color='white'>
+                    <BreadcrumbItem isCurrentPage>
+                        <BreadcrumbLink href='#'>Home</BreadcrumbLink>
+                    </BreadcrumbItem>
 
-                <BreadcrumbItem>
-                    <BreadcrumbLink href='#'>My Work</BreadcrumbLink>
-                </BreadcrumbItem>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href='#'>My Work</BreadcrumbLink>
+                    </BreadcrumbItem>
 
-                <BreadcrumbItem>
-                    <BreadcrumbLink href='#'>Resume</BreadcrumbLink>
-                </BreadcrumbItem>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href='#'>Resume</BreadcrumbLink>
+                    </BreadcrumbItem>
 
-                <BreadcrumbItem>
-                    <BreadcrumbLink href='#'>Contact</BreadcrumbLink>
-                </BreadcrumbItem>
-            </Breadcrumb>
-        </Flex>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href='#'>Contact</BreadcrumbLink>
+                    </BreadcrumbItem>
+                </Breadcrumb>
+            </Flex>
+            
+            <Flex display={{base: 'flex', sm:'flex', md:'flex', lg:'none'}}>
+                <Link href='#' color='white' fontSize='2xl'>t3chstop.github.io</Link>
+            </Flex>
+        </Box>
+        
     )
 }
